ad8ce38675f50d62fd8a3499824ec93ad7db0867
Aimlessly1012 <peco@pecodeMacBook-Pro.local>
2025-01-20 10:55:23 +0800

    Test commit3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import bundleAnalyzer from '@next/bundle-analyzer';
 
-export default nextConfig;
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true', //当环境变量ANALYZE为true时开启
+});
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
+  sassOptions: {
+    additionalData: '@import "@/assets/styles/index.scss";',
+  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'www.test.com',
+  //     },
+  //   ],
+  // },
+  poweredByHeader: false,
+};
+
+export default withBundleAnalyzer(nextConfig);
